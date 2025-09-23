c1dc540375d0eea700ceb8dc214dfb2b6aab18d7
Jade <jade.belloso@iesnervion.es>
2025-09-23 20:27:00 +0200

    Add files via upload

--- Proyect1/src/proyect1/Exercise09.java
+++ Proyect1/src/proyect1/Exercise09.java
@@ -12,25 +12,25 @@ public class Exercise09 {
         // Variable for the age
         int age;
         // Boolean variable to indicate if the user is of legal age
-        boolean mayoria;
+        boolean mayority;
 
         // Ask for the age
-        System.out.println("How old are you?");
+        System.out.println("¿Cuántos años tienes?");
         // Read the age
         age = sc.nextInt();
 
         // Check if the age is 18 or older
         if (age >= 18) {
-            mayoria = true; // If age is 18 or older, the user is of legal age
+            mayority = true; // If age is 18 or older, the user is of legal age
         } else {
-            mayoria = false; // If age is less than 18, the user is not of legal age
+            mayority = false; // If age is less than 18, the user is not of legal age
         }
 
         // Print the result based on the value of 'activo'
-        if (mayoria) {
-            System.out.println("The user is of legal age.");
+        if (mayority) {
+            System.out.println("El usuario es mayor de edad.");
         } else {
-            System.out.println("The user is not of legal age.");
+            System.out.println("El usuario no es mayor de edad.");
         }
 
         // Finally, close the scanner
