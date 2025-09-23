be5a59e503df69bb5ceda57d585392aed0511ed6
Jade <jade.belloso@iesnervion.es>
2025-09-23 19:54:04 +0200

    Update Exercise09.java

--- Proyect1/src/proyect1/Exercise09.java
+++ Proyect1/src/proyect1/Exercise09.java
@@ -4,41 +4,36 @@ import java.util.Scanner;
 
 public class Exercise09 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// We create a new Scanner
-		Scanner sc = new Scanner(System.in);
+        // Create a new Scanner
+        Scanner sc = new Scanner(System.in);
 
-		// Variable for the age
-		int age;
-		// We declare the boolean variable
-		boolean activo = true;
-		
-		
-		// We ask for the age
-		System.out.println("¿Cuántos años tienes?");
-		// And we read it
-		age = sc.nextInt();
-		
-		if (activo) { 
-		    System.out.println("El usuario es mayor de edad.");
-		} else {
-		    System.out.println("El usuario no es mayor de edad."); 
-		}
-				
-				
+        // Variable for the age
+        int age;
+        // Boolean variable to indicate if the user is of legal age
+        boolean mayoria;
 
-		
+        // Ask for the age
+        System.out.println("How old are you?");
+        // Read the age
+        age = sc.nextInt();
 
-		
-		
-		
-		
-		
-		
-		// And finally we close the scanner
-		sc.close();
+        // Check if the age is 18 or older
+        if (age >= 18) {
+            mayoria = true; // If age is 18 or older, the user is of legal age
+        } else {
+            mayoria = false; // If age is less than 18, the user is not of legal age
+        }
 
-	}
+        // Print the result based on the value of 'activo'
+        if (mayoria) {
+            System.out.println("The user is of legal age.");
+        } else {
+            System.out.println("The user is not of legal age.");
+        }
 
+        // Finally, close the scanner
+        sc.close();
+    }
 }
